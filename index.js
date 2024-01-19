59e1252a8e42c146c3c4a2cd939b27d486b185d9
jorodrig96 <jo.rodrig96@gmail.com>
2024-01-19 10:27:04 -0800

    added 2 commented  lines

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((req, res) =>{
+    //line below everything is inline. If taken out of line, youll get an error.
+    //it also prints it on localhost:2013 
     res.write('<h1>Fist server ever created.</h1><h2>Its not much. For now.</h2><img src="https://httpstatusdogs.com/img/206.jpg" alt="206">')
     res.end()
 })
