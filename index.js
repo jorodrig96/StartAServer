a0faaa977f417b8252a572618050f1c9140d4b7d
jorodrig96 <jo.rodrig96@gmail.com>
2024-01-19 10:24:57 -0800

    created a simple server file

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) =>{
+    res.write('<h1>Fist server ever created.</h1><h2>Its not much. For now.</h2><img src="https://httpstatusdogs.com/img/206.jpg" alt="206">')
+    res.end()
+})
+
+server.listen(2013, ()=>{
+    console.log('Greetings from your node server.')
+})
